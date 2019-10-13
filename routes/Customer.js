f967cfae102283e582819836bfb731e38c2548cb
Ruben Knapen <rubenknapen@gmail.com>
2019-10-13 16:07:29 +0200

    Added extra errors (like submitting data with empty strings)

--- routes/Customer.js
+++ routes/Customer.js
@@ -32,32 +32,43 @@ router.get('/:id?',function(req,res,next){
 });
  
 router.post('/',function(req,res,next){
-  customer.addCustomer(req.body,function(err){
-    if(err){
-      res.json(err);
-    }
-    else{
-      return res.status(200).send({
-          message: 'Customer has been succesfully created.'
-      });
-    }
-  });
+  if(req.body.email == "" || req.body.name == ""){
+    return res.status(400).send({
+      message: 'Missing email or name.'
+    });
+  } else {
+    customer.addCustomer(req.body,function(err){
+      if(err){
+        res.json(err);
+      } else {
+        return res.status(200).send({
+            message: 'Customer has been succesfully created.'
+        });
+      }
+    });
+  }
 });
 
 router.put('/:id',function(req,res,next){ 
   customer.updateCustomer(req.params.id,req.body,function(err,rows){     
-    if(err){
-      res.json(err);
-    }
-    else{
-      if(rows.affectedRows == 0){
-        return res.status(404).send({
-          message: 'This customer does not exist.'
-        });
-      } else {
-        return res.status(200).send({
-            message: 'Customer has been succesfully updated.'
-        });
+    if(req.body.email == "" || req.body.name == ""){
+      return res.status(400).send({
+        message: 'Missing email or name.'
+      });
+    } else {
+      if(err){
+        res.json(err);
+      }
+      else{
+        if(rows.affectedRows == 0){
+          return res.status(404).send({
+            message: 'This customer does not exist.'
+          });
+        } else {
+          return res.status(200).send({
+              message: 'Customer has been succesfully updated.'
+          });
+        }
       }
     }
   });
